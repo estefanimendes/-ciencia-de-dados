cb104dc90fe3a75ebc38cbcd256865ad8b393072
estefanimendes <127804911+estefanimendes@users.noreply.github.com>
2024-09-18 19:32:16 -0300

    a

--- Index.html/graficos/informacoesGlobais.js
+++ Index.html/graficos/informacoesGlobais.js
@@ -3,7 +3,9 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const paragrafo document.createElement('p')
+  paragrafo.classList.add('graficos-container__texto')
+  
 }
 
-vizualizarInformacoesGlobais() 
+vizualizarInformacoesGlobais()
