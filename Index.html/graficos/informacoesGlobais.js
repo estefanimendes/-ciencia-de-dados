cfe03777fdb53b98120b593ba10c912f39e59555
estefanimendes <127804911+estefanimendes@users.noreply.github.com>
2024-10-15 21:29:07 -0300

    hh

--- Index.html/graficos/informacoesGlobais.js
+++ Index.html/graficos/informacoesGlobais.js
@@ -8,14 +8,10 @@ async function vizualizarInformacoesGlobais() {
     const horas = parseInt(dados.tempo_medio)
     const minutos = (dados.tempo_medio - horas) * 100
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.`
 
-    const paragrafo document.createElement('p')
-  paragrafo.classList.add('graficos-container__texto')
-  paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.`<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
-  const container = document.getElementById('graficos-container')
-  console.log(paragrafo)
-  const container = document.getElementById('graficos-container')
-  container.appendChild(paragrafo)
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
-
-vizualizarInformacoesGlobais()
